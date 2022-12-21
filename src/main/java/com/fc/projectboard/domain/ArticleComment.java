458550401580ae68a09c0ad15fef9af3417cacf7
tudy <jhh0207@hanmail.net>
2022-12-21 14:57:50 +0900

    #21 - 회원 계정 관련 엔티티 수정 및 INSERT 쿼리 수정

--- src/main/java/com/fc/projectboard/domain/ArticleComment.java
+++ src/main/java/com/fc/projectboard/domain/ArticleComment.java
@@ -14,7 +14,7 @@ import java.time.LocalDateTime;
 import java.util.Objects;
 
 @Getter
-@ToString(callSuper = true)
+@ToString
 @Table(indexes = {
         @Index(columnList = "content"),
         @Index(columnList = "createdAt"),
@@ -28,24 +28,19 @@ public class ArticleComment extends AuditingFields{
 
     @Setter @ManyToOne(optional = false)
     private Article article; // 게시글  (ID)
-
-    @Setter @ManyToOne(optional = false)
-    private UserAccount userAccount;
-
     @Setter @Column(nullable = false, length = 500)
     private String content; // 본문
 
     protected ArticleComment() {
     }
 
-    public ArticleComment(Article article, UserAccount userAccount, String content) {
+    private ArticleComment(Article article, String content) {
         this.article = article;
-        this.userAccount = userAccount;
         this.content = content;
     }
 
-    public static ArticleComment of(Article article, UserAccount userAccount,String content) {
-        return new ArticleComment(article, userAccount, content);
+    public static ArticleComment of(Article article, String content) {
+        return new ArticleComment(article, content);
     }
 
     @Override
